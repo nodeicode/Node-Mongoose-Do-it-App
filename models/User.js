3b93a9452ce1462ef2dcffb6ecdf419dd718230d
Lohit Ary <lgopik2@uic.edu>
2018-07-15 21:19:39 +0530

    Schema almost done only the pre save hooks and some middleware left :)

--- models/User.js
+++ models/User.js
@@ -1,1 +1,26 @@
 //User Schema and a pre-save (TO Be configured Later)
+//Imp to remember points in comments bois!!
+
+const mongoose = require('mongoose')
+const slug = require('slugs')
+const validator = require('validator')
+const mongooseLocal = require('passport-local-mongoose')
+
+var UserSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        trim:true,
+        required: "Please Enter An Email",
+        validate: [validator.isEmail(),validator.NormalizeEmail()], //Always validata a array of methods [ ] not objects { }
+    },
+    Name:{
+        type:String,
+        required:"Please Enter ur Name!",
+        trim:true,
+    }
+})
+
+UserSchema.plugin(mongooseLocal)
+
+
+module.exports = mongoose.model(UserSchema,User)
