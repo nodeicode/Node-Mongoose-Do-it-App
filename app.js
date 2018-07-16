ece9b2f32a37a40f515dc574bb8d58c0f91bc640
Lohit Ary <lgopik2@uic.edu>
2018-07-16 23:23:53 +0530

    basic done!!

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //requiring all app dependencies and configuring middleware
+const express = require('express')
+const session = require('express-session')
+const routes = require('./routes/index.js')
+const  validator = require('express-validator')
+const flash = require('connect-flash')
+const Parser = require('cookie-parser')
+const path = require('path')
+
+const app  = express()
+
+app.use(validator())
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','pug')
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use(flash())
+
+app.use(Parser())
+
+app.use(routes)
+
+//app.use passport sesson and initialize left!
+
+module.exports = app
