0986ea777852d5c15ef266be07b7f28a7efd4cf8
Lohit Ary <lgopik2@uic.edu>
2018-07-16 23:24:12 +0530

    still gotta connect to a mongodb comapss server :<

--- start.js
+++ start.js
@@ -1,1 +1,7 @@
 //connect to mongo and start up the ports
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const express = require('express')
+const app = require('./app')
+
+const server = app.listen(3000,()=>{console.log("EXPRESS PORT RUNNING AT -> 3000")})
