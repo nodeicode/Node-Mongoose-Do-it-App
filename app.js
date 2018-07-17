9a9de1bac81a8dd4b09332cfde24c42b94cc0025
Lohit Ary <lgopik2@uic.edu>
2018-07-17 20:09:21 +0530

    more changes to be done!

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const app  = express()
 
 app.use(validator())
 
-app.set('views',path.join(__dirname,'views'))
+app.set('views',path.join(__dirname,'./views'))
 app.set('view engine','pug')
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'./public')))
 
 app.use(flash())
 
