6f475d73b0cf4c314736dc1ea9cc716b11530748
Lohit Ary <lgopik2@uic.edu>
2018-07-18 17:23:31 +0530

    almost done or  maybe its my imagination :(

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router  = express.Router()
-//const controller  = require('./controllers/UserController.')
+//const controller  = require('./controllers/authController.')
 //for params use router.param@)
 
 router.get('/',(req,res)=>{
-    res.render('../views/home')
+    res.render('../views/Home.pug')
 })
 module.exports = router
