d0dbe74eabcc2881cc551a5d7e6145c33eef2e44
Lohit Ary <lgopik2@uic.edu>
2018-07-18 17:22:37 +0530

    basic and passport done still middleware left

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const  validator = require('express-validator')
 const flash = require('connect-flash')
 const Parser = require('cookie-parser')
 const path = require('path')
+const passport = require('passport')
+const Mongo = require('connect-mongo')(session)
 
 const app  = express()
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(validator())
 
 app.set('views',path.join(__dirname,'./views'))
 app.set('view engine','pug')
 
-app.use(express.static(path.join(__dirname,'./public')))
+app.use(express.static(path.join(__dirname,'/public')))
 
 app.use(flash())
 
