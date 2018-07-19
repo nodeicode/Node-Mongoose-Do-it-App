60ecfa8127ba0a786d70c8a3320ebb2b7d62587e
Lohit Ary <lgopik2@uic.edu>
2018-07-19 17:43:32 +0530

    route to regiter and login finally!!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router  = express.Router()
-//const controller  = require('./controllers/authController.')
+const controller  = require('../controllers/authControllers')
 //for params use router.param@)
 
 router.get('/',(req,res)=>{
     res.render('../views/Home.pug')
 })
+
+router.post('/register',(req,res)=>{
+    controller.register,
+    controller.login
+})
+
 module.exports = router
