6e982898e715cbb2b78fc25ffa2e73068917aed5
Lohit Ary <lgopik2@uic.edu>
2018-07-19 17:42:55 +0530

    mistake in validators

--- models/User.js
+++ models/User.js
@@ -5,22 +5,25 @@ const mongoose = require('mongoose')
 const slug = require('slugs')
 const validator = require('validator')
 const mongooseLocal = require('passport-local-mongoose')
+const errorhandler = require('mongoose-mongodb-errors')
 
 var UserSchema = new mongoose.Schema({
     email:{
         type:String,
         trim:true,
+        unique:true,
         required: "Please Enter An Email",
-        validate: [validator.isEmail(),validator.NormalizeEmail()], //Always validata a array of methods [ ] not objects { }
+        validate: [validator.isEmail,validator.NormalizeEmail], //Always validata a array of methods [ ] not objects { }
     },
     Name:{
         type:String,
         required:"Please Enter ur Name!",
         trim:true,
-    }
+    },
 })
 
-UserSchema.plugin(mongooseLocal)
+UserSchema.plugin(mongooseLocal,{usernameFeild:'email'})
+UserSchema.plugin(errorhandler)
 
 
-module.exports = mongoose.model(UserSchema,User)
+module.exports = mongoose.model('User',UserSchema)
