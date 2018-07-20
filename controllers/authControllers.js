76add3a2ca00381dbdf7ffbce7788eb1e8a2db5f
Lohit Ary <lgopik2@uic.edu>
2018-07-20 20:01:32 +0530

    basic controller function for form started still need work!

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,1 +1,10 @@
 //use expres-validator and create a function to validate regster forms and login requests
+const mongoose  = require('mongoose')
+const User = mongoose.model('User')
+const promisify = require('es6-promisify')
+const passport = require('passport')
+
+exports.registerNlogin = (req,res)=>{
+req.sanitizeBody('name')
+req.checkBody('name','We need Your Name!').notEmpty()
+}
