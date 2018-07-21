43cf5a7cded21aa526b390faf41c7c47554ebc45
Lohit Ary <lgopik2@uic.edu>
2018-07-21 18:58:31 +0530

    registration and login finished !!! need error handlers still

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,7 +4,38 @@ const User = mongoose.model('User')
 const promisify = require('es6-promisify')
 const passport = require('passport')
 
-exports.registerNlogin = (req,res)=>{
+exports.validateRegister = (req,res,next)=>{
 req.sanitizeBody('name')
 req.checkBody('name','We need Your Name!').notEmpty()
+req.checkBody('email').isEmail()
+req.sanitizeBody('email').normalizeEmail({
+    gmail_remove_subaddress:false,
+    gmail_remove_dots:false,
+})
+req.checkBody('password').notEmpty()
+var errors  = req.validationErrors()
+if(errors){req.flash('error',errors)
+return
 }
+next()
+}
+
+exports.register= async (req,res,next) =>  {
+const user  = new User({email:req.body.email,name:req.body.name})
+console.log(req.body.name)
+var register = promisify(User.register,User)
+await register(user,req.body.password)
+next()
+}
+
+exports.login = passport.authenticate('local',{
+        failureRedirect:'/',
+        failureFlash:'Failed Login Try Again!',
+        successRedirect:'/account',
+        successFlash:'Loggin In !!'
+    })
+
+exports.account = (req,res)=>{
+res.render('../views/acc.pug')
+}
+
