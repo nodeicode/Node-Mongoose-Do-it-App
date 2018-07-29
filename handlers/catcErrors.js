7b05336ed8f69359371dfa9994138b073d1bba69
Lohit Ary <lgopik2@uic.edu>
2018-07-29 21:24:56 +0530

    catching errors and display almost done!

---
+++ handlers/catcErrors.js
@@ -0,0 +1,21 @@
+//catch and handle all the errors
+//gotta be updated!!!!
+exports.catchErrors = (fn) => {
+return function(req,res,next){
+     return  fn(req,res,next).catch(next)
+}
+}
+
+//404
+exports.ntFnd = (req,res,next)=>{
+    req.flash('error','404 Page not found Your Now at Home!')
+    res.redirect('/')
+}
+
+//flash errors
+exports.flashes = (err,req,res,next)=>{
+    if(!err)return next
+    req.flash(err)
+    console.log(err)
+    res.redirect('/')
+}
