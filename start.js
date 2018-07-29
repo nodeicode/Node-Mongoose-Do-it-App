6c36ea16afa02f6321761db3a4772f3f2a89f66f
Lohit Ary <lgopik2@uic.edu>
2018-07-29 21:23:39 +0530

    fixed the connection log

--- start.js
+++ start.js
@@ -8,8 +8,9 @@ require('dotenv').config({path:'.env'})
 
 mongoose.connect(process.env.MONGODB,{useNewUrlParser:true})
 mongoose.Promise = global.Promise
-mongoose.connection.on('connection',()=>{
-    console.log('Connected to Mongoose Server !!! ')
-})
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', function() {
+  console.log('connected to mongdb SERVER')
+});
 
 const server = app.listen(process.env.PORT,()=>{console.log("Express Running At Port -> "+process.env.PORT)})
