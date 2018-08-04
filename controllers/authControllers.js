c42674d2b9dbb76dc9525548f18f9385eeabd072
Lohit Ary <lgopik2@uic.edu>
2018-08-04 22:44:56 +0530

    all prettier and fixed up the duplicate key issue!!

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,60 +1,61 @@
 //use expres-validator and create a function to validate regster forms and login requests
-const mongoose  = require('mongoose');
-const User = mongoose.model('User');
-const promisify = require('es6-promisify');
-const passport = require('passport');
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
-exports.home = (req,res)=>{
-    res.render('Home');
+exports.home = (req, res) => {
+  res.render("Home");
 };
 
-exports.validateRegister = (req,res,next)=>{
-req.sanitizeBody('name');
-req.checkBody('name','We need Your Name!').notEmpty();
-req.checkBody('email','That email is invalid!').isEmail();
-req.sanitizeBody('email').normalizeEmail({
-    gmail_remove_subaddress:false,
-    remove_extension:false,
-    all_lowercase:false,
-    gmail_remove_dots:false,
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody("name");
+  req.checkBody("name", "We need Your Name!").notEmpty();
+  req.checkBody("email", "That email is invalid!").isEmail();
+  req.sanitizeBody("email").normalizeEmail({
+    gmail_remove_subaddress: false,
+    remove_extension: false,
+    gmail_remove_dots: false
+  });
+  req.sanitizeBody("password");
+  req.checkBody("password", "Where is ur password?").notEmpty();
+  const errors = req.validationErrors();
+  if (errors) {
+    req.flash("error", errors.map(err => err.msg));
+    res.render("Home", { body: req.body, flashes: req.flash() });
+    return;
+  }
+  next();
+};
+
+exports.register = async (req, res, next) => {
+  var user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10)
+  });
+  await user.save();
+  console.log("registered!!")
+  next();
+};
+
+exports.login = passport.authenticate("local", {
+  failureRedirect: "/",
+  failureFlash: "Failed Login Try Again!",
+  successRedirect: "/account",
+  successFlash: "Logged In !!👌"
 });
-req.sanitizeBody('password');
-req.checkBody('password','Where is ur password?').notEmpty();
-req.checkBody('password','Ur password should have letters AND numbers !!🤗 ')
-const  errors  = req.validationErrors();
-if(errors){
-req.flash('error',errors.map(err => err.msg));
-res.render('Home',  {body:req.body,flashes:req.flash()});
-return
-}
-next();
-};
 
-exports.register= async (req,res,next) =>  {
-var user  = new User({name:req.body.name,email:req.body.email,password:bcrypt.hashSync(req.body.password,10)});
-console.log(req.body.email)
-await user.save()
-console.log("registered BOIIS");
-next();
-};
-
-exports.login = passport.authenticate('local',{
-        failureRedirect:'/',
-        failureFlash:'Failed Login Try Again!',
-        successRedirect:'/account',
-        successFlash:'Logged In !!👌'
-    });
-
-exports.isLogged = (req,res,next)=>{
-    if(req.isAuthenticated()){
+exports.isLogged = (req, res, next) => {
+  if (req.isAuthenticated()) {
     next();
-    return};
-req.flash('error','Looks like ur not logged in plz log in !🤷‍');
-res.redirect('/');
+    return;
+  }
+  req.flash("error", "Looks like ur not logged in plz log in !🤷‍");
+  res.redirect("/");
 };
 
-exports.account = (req,res)=>{
-res.render('../views/acc.pug');
+exports.account = (req, res) => {
+  res.render("../views/acc.pug");
 };
-
