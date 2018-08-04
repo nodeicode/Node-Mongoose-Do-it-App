fe853b443f60b364e39d040e51333c18061e074a
Lohit Ary <lgopik2@uic.edu>
2018-08-04 22:45:15 +0530

    all prettier!

--- models/User.js
+++ models/User.js
@@ -1,38 +1,40 @@
 //User Schema and a pre-save (TO Be configured Later)
 //Imp to remember points in comments bois!!
 
-const mongoose = require('mongoose')
-const slug = require('slugs')
-const Schema = mongoose.Schema
-mongoose.Promise = global.Promise
-const validator = require('validator')
-const errorhandler = require('mongoose-mongodb-errors')
+const mongoose = require("mongoose");
+const slug = require("slugs");
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const validator = require("validator");
+const errorhandler = require("mongoose-mongodb-errors");
 
 const UserSchema = new Schema({
-    name:{
-        type:String,
-        required:'Please Enter ur Name!😎✨',
-        trim:true,
-    },
-    email:{
-        trim:true,
-        type:String,
-        unique:true,
-        lowercase:true,
-        required:'Please Enter an Email!',
-        validate:[validator.isEmail,'Invalid Email!! 😮']
-    },
-    password:{
-        trim:true,
-        type:String,
-        required:'Please Enter a password',
-        validate:{validator:function(password){
-            return /^\$2[ayb]\$.{56}$/.test(password); //regx for checking a bcrypt salted hash!!
-        },msg:'Check database! password is not a hash 🤦‍'}
+  name: {
+    type: String,
+    required: "Please Enter ur Name!😎✨",
+    trim: true
+  },
+  email: {
+    trim: true,
+    type: String,
+    unique: false,
+    lowercase: true,
+    required: "Please Enter an Email!",
+    validate: [validator.isEmail, "Invalid Email!! 😮"]
+  },
+  password: {
+    trim: true,
+    type: String,
+    required: "Please Enter a password",
+    validate: {
+      validator: function(password) {
+        return /^\$2[ayb]\$.{56}$/.test(password); //regx for checking a bcrypt salted hash!!
+      },
+      msg: "Check database! password is not a hash 🤦‍"
     }
-})
+  }
+});
 
-UserSchema.plugin(errorhandler)
+UserSchema.plugin(errorhandler);
 
-
-module.exports = mongoose.model('User',UserSchema)
+module.exports = mongoose.model("User", UserSchema);
--- start.js
+++ start.js
@@ -1,16 +1,24 @@
 //connect to mongo and start up the ports
-const mongoose = require('mongoose')
-const flash = require('connect-flash')
-const express = require('express')
-require('./models/User.js')
-const app = require('./app')
-require('dotenv').config({path:'.env'})
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const express = require("express");
+require("./models/User.js");
+const app = require("./app");
+require("dotenv").config({ path: ".env" });
 
-mongoose.connect(process.env.MONGODB,{useNewUrlParser:true})
-mongoose.Promise = global.Promise
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-mongoose.connection.once('open', function() {
-  console.log('connected to mongdb SERVER')
+mongoose.connect(
+  process.env.MONGODB,
+  { useNewUrlParser: true }
+);
+mongoose.Promise = global.Promise;
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "connection error:")
+);
+mongoose.connection.once("open", function() {
+  console.log("connected to mongdb SERVER");
 });
 
-const server = app.listen(process.env.PORT,()=>{console.log("Express Running At Port -> "+process.env.PORT)})
+const server = app.listen(process.env.PORT, () => {
+  console.log("Express Running At Port -> " + process.env.PORT);
+});
