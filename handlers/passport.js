0a0c6db88aa612c4f55cfe5f582647ad1fab68d3
Lohit Ary <lgopik2@uic.edu>
2018-08-04 22:54:24 +0530

    all prettier!

--- handlers/passport.js
+++ handlers/passport.js
@@ -1,30 +1,37 @@
-const passport = require('passport');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const  LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const LocalStrategy = require("passport-local").Strategy;
 
-passport.use(new LocalStrategy({
-  usernameField:'email',
-  passwordField:'password'
-},
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password"
+    },
     function(username, password, done) {
-        User.findOne({ username: username }, function (err, user) {
-          if (err) { return done(err); }
-          if (!user) {
-            return done(null, false, { message: 'Incorrect username.' });
-          }
-          /*if (!user.validPassword(password)) {
-            return done(null, false, { message: 'Incorrect password.' });
-          }*/
-          return done(null, user);
-        });
-      }
-));
+      User.findOne({ username: username }, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username." });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+
+        return done(null, user);
+      });
+    }
+  )
+);
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
-passport.deserializeUser( function(id, done) {
+passport.deserializeUser(function(id, done) {
   User.findById(id, function(err, user) {
-  done(err, user);
-})});
+    done(err, user);
+  });
+});
